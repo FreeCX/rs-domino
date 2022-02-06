5ba65ac74f7c5095d5f806191a090ab454539539
Alexey Golubev <dr.freecx@gmail.com>
2022-02-06 13:38:46 +0300

    chore: tiny fix

--- build/pack.rs
+++ build/pack.rs
@@ -12,7 +12,7 @@ pub fn pack_resource() -> Result<()> {
     let mut source_code = String::new();
     source_code.push_str(GEN_HEADER);
     for (id, name) in bundle.build_index() {
-        source_code.push_str(&format!("pub static {name}: u16 = {id};\n"));
+        source_code.push_str(&format!("pub const {name}: u16 = {id};\n"));
     }
     // and write it to file
     File::create("src/resource.rs").and_then(|mut file| file.write_all(source_code.as_bytes()))?;
